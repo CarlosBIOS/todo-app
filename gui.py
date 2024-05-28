1d4c324e377f7bfa6ebb45f4f0347976e51b224d
Carlos Monteiro <cmmonteiro40@gmail.com>
2024-05-28 12:15:29 +0100

    Acrescentei algumas informaçoes sobre FreeSimpleGui

--- gui.py
+++ gui.py
@@ -1,5 +1,8 @@
-import functions
+# FreeSimpleGUI is only able to create desktop GUIs. To make web apps, you need to use a Python web framework. The most
+# popular web frameworks are Django, Flask, and Streamlit. In fact, Python is way better for building web apps. We will
+# eventually build a To-Do List web app on Day 19!!!
 from FreeSimpleGUI import Text, Window, InputText, Button
+import functions
 
 label = Text('Type in a To-Do')
 input_box = InputText(tooltip='Enter todo')
