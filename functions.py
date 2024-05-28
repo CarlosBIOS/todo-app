f50fcc0ccb8c17dcaba558b3b80ee473714b2024
Carlos Monteiro <cmmonteiro40@gmail.com>
2024-05-28 19:59:43 +0100

    Melhorei o docstring e acrescentei um parametro, onde se colocar algo, vai acrescentar algum item na lista

--- functions.py
+++ functions.py
@@ -1,14 +1,20 @@
+# Fazer uma função onde faz o get_todo e write_todo simultaneamente!!
 FILEPATH = 'Files/todo.txt'
 
 
-def get_todos(filepath: str = FILEPATH) -> list:
+def get_todos(filepath: str = FILEPATH, item: str = None) -> list:
     """
-    Read a text file and return the list of to-do items
+    Read a text file and return the list of to-do items.
+
+    If `item` is define, então vão colocá-la na lista of to-do
     :param filepath: O ficheiro txt que quero ler
+    :param item:
     :return: a list onde cada elemento é um parágrafo
     """
     with open(filepath) as file_local:
         todos_local: list = file_local.readlines()
+        if item:
+            todos_local.append(item)
     return todos_local
 
 
