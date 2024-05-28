4e5cee1fc8471ed7ceb493a403cc3b44b66e5756
Carlos Monteiro <cmmonteiro40@gmail.com>
2024-05-28 11:12:33 +0100

    Acrescentei o docstring em ambas as funções

--- functions.py
+++ functions.py
@@ -1,14 +1,22 @@
 FILEPATH = 'Files/todo.txt'
 
 
-def get_todos(filepath=FILEPATH):
-    """Read a text file and return the list of to-do items"""
+def get_todos(filepath: str = FILEPATH) -> list:
+    """
+    Read a text file and return the list of to-do items
+    :param filepath: O ficheiro txt que quero ler
+    :return: a list onde cada elemento é um parágrafo
+    """
     with open(filepath) as file_local:
-        todos_local = file_local.readlines()
+        todos_local: list = file_local.readlines()
     return todos_local
 
 
-def write_todos(todos_arg, filepath=FILEPATH):
-    """Write the to-do items list in the text files"""
+def write_todos(todos_arg, filepath=FILEPATH) -> None:
+    """
+    Write the to-do items list in the text files
+    :param todos_arg: The items that I want to put in the `filepath`
+    :param filepath: O ficheiro onde quero escrever
+    """
     with open(filepath, 'w') as file:
         file.writelines(todos_arg)
