ea76a57775075a2f1e988242f0601b5db92d38a1
Carlos Monteiro <cmmonteiro40@gmail.com>
2024-05-28 20:04:24 +0100

    Agora dá para acrescentar e modificar o documento txt

--- gui.py
+++ gui.py
@@ -1,13 +1,23 @@
 # FreeSimpleGUI is only able to create desktop GUIs. To make web apps, you need to use a Python web framework. The most
 # popular web frameworks are Django, Flask, and Streamlit. In fact, Python is way better for building web apps. We will
 # eventually build a To-Do List web app on Day 19!!!
-from FreeSimpleGUI import Text, Window, InputText, Button
+from FreeSimpleGUI import Text, Window, InputText, Button, WINDOW_CLOSED
 import functions
 
 label = Text('Type in a To-Do')
-input_box = InputText(tooltip='Enter todo')
+input_box = InputText(tooltip='Enter todo', key='todo')
 button = Button('Add')
 
-my_window = Window('My To-Do App', layout=[[label], [input_box, button]])
-my_window.read()
+my_window = Window('My To-Do App', layout=[[label], [input_box, button]], font=('Helvetica', 20))
+while True:
+    event, values = my_window.read()
+    print(event, values)
+    match event:
+        case 'Add':
+            todos: list = functions.get_todos(item=values['todo'] + '\n')
+            functions.write_todos(todos)
+
+        case WINDOW_CLOSED:
+            break
+
 my_window.close()
