b626599c4ecdc40d6a359e3f87479cfe135239f0
Carlos Monteiro <cmmonteiro40@gmail.com>
2024-05-29 12:29:17 +0100

    Acrescentei o edit button e list box ao my_window

--- gui.py
+++ gui.py
@@ -1,15 +1,18 @@
 # FreeSimpleGUI is only able to create desktop GUIs. To make web apps, you need to use a Python web framework. The most
 # popular web frameworks are Django, Flask, and Streamlit. In fact, Python is way better for building web apps. We will
 # eventually build a To-Do List web app on Day 19!!!
-from FreeSimpleGUI import Text, Window, InputText, Button
+from FreeSimpleGUI import Text, Window, InputText, Button, LBox
 
 import functions
 
 label = Text('Type in a To-Do')
 input_box = InputText(tooltip='Enter todo', key='todo')
-button = Button('Add')
+add_button = Button('Add')
+list_box = LBox(values=functions.get_and_write_todos(), enable_events=True, size=(45, 10), key='todos')
+edit_button = Button('Edit')
 
-my_window = Window('My To-Do App', layout=[[label], [input_box, button]], font=('Helvetica', 20))
+my_window = Window('My To-Do App', layout=[[label], [input_box, add_button], [list_box, edit_button]],
+                   font=('Helvetica', 20))
 while True:
     event, values = my_window.read()
     print(event, values)
