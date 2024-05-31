208b4dfb2b63fac7d49d6f00f12116f951a35093
Carlos Monteiro <cmmonteiro40@gmail.com>
2024-05-31 12:32:43 +0100

    Melhorei ao máximo

--- gui.py
+++ gui.py
@@ -1,9 +1,12 @@
 # FreeSimpleGUI is only able to create desktop GUIs. To make web apps, you need to use a Python web framework. The most
 # popular web frameworks are Django, Flask, and Streamlit. In fact, Python is way better for building web apps. We will
 # eventually build a To-Do List web app on Day 19!!!
-from FreeSimpleGUI import Text, Window, InputText, Button, Listbox, popup
+from FreeSimpleGUI import Text, Window, InputText, Button, Listbox, popup, theme, WINDOW_CLOSED
 import functions
+import time
 
+theme('NeonBlue1')
+clock = Text('', key='clock')
 label = Text('Type in a To-Do')
 input_box = InputText(tooltip='Enter todo', key='todo')
 add_button = Button('Add')
@@ -13,12 +16,13 @@ exit_button = Button('Exit')
 complete_button = Button('Complete')
 
 my_window = Window('My To-Do App',
-                   layout=[[label], [input_box, add_button], [list_box, edit_button, complete_button], [exit_button]],
+                   layout=[[clock], [label], [input_box, add_button], [list_box, edit_button, complete_button],
+                           [exit_button]],
                    font=('Helvetica', 20))
 
 while True:
-    event, values = my_window.read()
-    print(event, values)
+    event, values = my_window.read(timeout=200)
+    my_window['clock'].update(value=time.strftime('%d %b %Y, %H:%M:%S'))
     match event:
         case 'Add':
             if values['todo']:
@@ -48,7 +52,5 @@ while True:
             exit()  # Vantagem de usar esta função, é que dps de executá-la, o programa fecha-se completamente, então já
             # não é preciso o my_window.close()
 
-        case WIN_CLOSED:
-            exit()
-
 # Acrscentar as que já foram feitas!
+# datas
